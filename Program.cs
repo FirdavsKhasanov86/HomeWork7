b7110cfd25e03b911ff849cfa7f385391233e78a
user <user@Williams-iMac.local>
2020-04-03 23:25:39 +0500

    HW7

--- Program.cs
+++ Program.cs
@@ -6,7 +6,55 @@ namespace HomeWork7
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            //Задание 1.
+            // Console.Write("side1:"); 
+            // double side1 = double.Parse(Console.ReadLine());
+            // Console.Write("side2:"); 
+            // double side2= double.Parse(Console.ReadLine());
+            
+            // Rectangle rec = new Rectangle(); 
+                
+            //     rec.AreaCalculator(side1,side2);
+            //     rec.PerimeterCalculator(side1,side2);
+
+          //----------------------------------------------------//
+            
+
+          //-------------------------------------------------------//
+
+          
         }
     }
+
+    //Задание 1.
+    //  class Rectangle
+    //  {
+    //         public double side1 {get;set;}
+    //         public double side2{get;set;} 
+    //         public double area {get { return side1 * side2; } }
+    //         public double perimeter{get { return 2 * (side1 + side2); }}
+    
+
+    
+    //          public void AreaCalculator(double side1, double side2)
+    //          {
+    //             Console.WriteLine($"Ploshad pryamougolnika s = {side1 * side2}");
+    //          }
+    //          public void PerimeterCalculator(double side1, double side2)
+    //          {
+    //           Console.WriteLine($"Perimetr pryamogolnika P = {(side1 * side2)*2}");
+    //          }
+    //  }
+
+    //------------------------------------------------------------------------------------//
+    
+
+    //---------------------------------------------------------------//
+
+    
+    
+    
+    
+
 }
+
